b48bbfc6e2b70c8850ff9c8b1214e8576c0cfaba
gibus <gibus@easter-eggs.com>
2019-02-01 22:37:50 +0100

    Provide naturalSort as an alternative to Intl.Collator.compare to please phantomJS

--- static/js/conditional-customfields.js
+++ static/js/conditional-customfields.js
@@ -50,26 +50,53 @@ function activate_datepicker() {
     });
 }
 
-function compare_str_or_num(a, b) {
-    if (isNaN(a) || isNaN(b)) {
-        if (a < b) {
-            return -1;
-        } else if (a == b) {
-            return 0;
-        } else {
-            return 1;
-        }
-    } else {
-        var num_a = Number(a);
-        var num_b = Number(b);
-        if (num_a < num_b) {
-            return -1;
-        } else if (num_a == num_b) {
-            return 0;
-        } else {
-            return 1;
-        }
+function naturalSort(a, b, lang) {
+    if (window.Intl) {
+        if(lang === "") lang = navigator.language;
+        return window.Intl.Collator(lang, {sensitivity: 'base', numeric: true}).compare(a.toString(), b.toString());
     }
+
+    /*
+     * Natural Sort algorithm for Javascript - Version 0.7 - Released under MIT license
+     * Author: Jim Palmer (based on chunking idea from Dave Koelle)
+     * http://www.overset.com/2008/09/01/javascript-natural-sort-algorithm-with-unicode-support/
+     */
+    var re = /(^-?[0-9]+(\.?[0-9]*)[df]?e?[0-9]?$|^0x[0-9a-f]+$|[0-9]+)/gi,
+        sre = /(^[ ]*|[ ]*$)/g,
+        dre = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/,
+        hre = /^0x[0-9a-f]+$/i,
+        ore = /^0/,
+        i = function(s) { return naturalSort.insensitive && (''+s).toLowerCase() || ''+s },
+        // convert all to strings strip whitespace
+        x = i(a).replace(sre, '') || '',
+        y = i(b).replace(sre, '') || '',
+        // chunk/tokenize
+        xN = x.replace(re, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
+        yN = y.replace(re, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
+        // numeric, hex or date detection
+        xD = parseInt(x.match(hre)) || (xN.length != 1 && x.match(dre) && Date.parse(x)),
+        yD = parseInt(y.match(hre)) || xD && y.match(dre) && Date.parse(y) || null,
+        oFxNcL, oFyNcL;
+    // first try and sort Hex codes or Dates
+    if (yD)
+        if ( xD < yD ) return -1;
+    else if ( xD > yD ) return 1;
+    // natural sorting through split numeric strings and default strings
+    for(var cLoc=0, numS=Math.max(xN.length, yN.length); cLoc < numS; cLoc++) {
+        // find floats not starting with '0', string or 0 if not defined (Clint Priest)
+        oFxNcL = !(xN[cLoc] || '').match(ore) && parseFloat(xN[cLoc]) || xN[cLoc] || 0;
+        oFyNcL = !(yN[cLoc] || '').match(ore) && parseFloat(yN[cLoc]) || yN[cLoc] || 0;
+        // handle numeric vs string comparison - number < string - (Kyle Adams)
+        if (isNaN(oFxNcL) !== isNaN(oFyNcL)) { return (isNaN(oFxNcL)) ? 1 : -1; }
+        // rely on string comparison if different types - i.e. '02' < 2 != '02' < '2'
+        else if (typeof oFxNcL !== typeof oFyNcL) {
+            oFxNcL += '';
+            oFyNcL += '';
+        }
+        if (oFxNcL < oFyNcL) return -1;
+        if (oFxNcL > oFyNcL) return 1;
+    }
+    return 0;
 }
 
 function condition_is_met(conditionedby_vals, condition_vals, condition_op, lang) {
@@ -93,19 +120,18 @@ function condition_is_met(conditionedby_vals, condition_vals, condition_op, lang
                         return !condition_met;
                     }
                 } else if (condition_op == "less than") {
-                    //if (new Intl.Collator(lang, {sensitivity: 'base', numeric: true}).compare(condition_vals[i].toString(), conditionedby_vals[j].toString()) <= 0) {
-                    //}
-                    if (compare_str_or_num(condition_vals[i], conditionedby_vals[j]) <= 0) {
+                    if (naturalSort(condition_vals[i], conditionedby_vals[j], lang) <= 0) {
                         return !condition_met;
                     }
                 } else if (condition_op == "greater than") {
-                    if (new Intl.Collator(lang, {sensitivity: 'base', numeric: true}).compare(condition_vals[i].toString(), conditionedby_vals[j].toString()) >= 0) {
+                    if (naturalSort(condition_vals[i], conditionedby_vals[j], lang) >= 0) {
                         return !condition_met;
                     }
                 } else if (condition_op == "between") {
-                    if (j == 0 && new Intl.Collator(lang, {sensitivity: 'base', numeric: true}).compare(condition_vals[i].toString(), conditionedby_vals[j].toString()) < 0) {
+                    var comp = naturalSort(condition_vals[i], conditionedby_vals[j], lang);
+                    if (j == 0 && comp < 0) {
                         return !condition_met;
-                    } else if (j == 1 && new Intl.Collator(lang, {sensitivity: 'base', numeric: true}).compare(condition_vals[i].toString(), conditionedby_vals[j].toString()) > 0) {
+                    } else if (j == 1 && comp > 0) {
                         return !condition_met;
                     }
                 }
