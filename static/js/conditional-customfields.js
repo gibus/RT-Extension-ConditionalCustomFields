a05a5e39c608e0349696ca4399cab3abcda2083d
gibus <gibus@easter-eggs.com>
2023-10-11 15:37:37 +0200

    Retrigger change on Dropdown Condition CF after inline form has been made visible

--- static/js/conditional-customfields.js
+++ static/js/conditional-customfields.js
@@ -259,3 +259,17 @@ function waitForElm(selector) {
         });
     });
 }
+
+function respondToVisibility(element, callback) {
+  var options = {
+    root: document.documentElement
+  }
+
+  var observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      callback(entry.intersectionRatio > 0);
+    });
+  }, options);
+
+  observer.observe(element);
+}
