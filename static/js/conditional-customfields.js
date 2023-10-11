a8cfd29f79e74d32ae8edb5e66e97a7b4e8c394a
gibus <gibus@easter-eggs.com>
2023-10-11 15:36:11 +0200

    Retrigger change on Dropdown Condition CF after bootstrap dropdown has made it visible

--- static/js/conditional-customfields.js
+++ static/js/conditional-customfields.js
@@ -239,3 +239,23 @@ function condition_is_met(condition_vals, cf_condition_vals, condition_op, lang)
 
     return condition_met;
 }
+
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
